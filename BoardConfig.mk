6b242da4a8636ba74b37212ba2e25302592895e9
Arian <arian.kulmer@web.de>
2021-11-07 19:17:23 +0100

    mars: Set bootloader board name
    Change-Id: Ied8de2941ccc455c7755f50e3843eadde59f1ad8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,9 @@ DEVICE_PATH := device/xiaomi/mars
 # Inherit from sm8350-common
 include device/xiaomi/sm8350-common/BoardConfigCommon.mk
 
+# Board
+TARGET_BOOTLOADER_BOARD_NAME := mars
+
 # Kernel
 TARGET_KERNEL_CONFIG += vendor/star_QGKI.config
 
