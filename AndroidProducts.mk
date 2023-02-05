8600270670f725653fc36d18afa394d1661fa044
FlowerSea0208 <zzp060208@gmail.com>
2023-02-05 11:50:10 +0800

    mars: Initial device tree
    Change-Id: I4ecd8b71c5c409ee37a22aa3b69367c1ffb99210

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_mars.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_mars-user \
+    lineage_mars-userdebug \
+    lineage_mars-eng
