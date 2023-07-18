crdroidandroid/android_device_xiaomi_venus

mars: Include device specific kernel config fragment
Change-Id: I057a34e502a26a7a0a73b394d37739e8d45679b8

BoardConfig.mk
@@ -9,5 +9,8 @@ DEVICE_PATH := device/xiaomi/mars
 # Inherit from sm8350-common
 include device/xiaomi/sm8350-common/BoardConfigCommon.mk
 
+# Kernel
+TARGET_KERNEL_CONFIG += vendor/mars_QGKI.config
+
 # Include proprietary files
 include vendor/xiaomi/mars/BoardConfigVendor.mk

LineageOS/android_device_xiaomi_mars

BoardConfig.mk
@@ -9,5 +9,8 @@ DEVICE_PATH := device/xiaomi/mars
 # Inherit from sm8350-common
 include device/xiaomi/sm8350-common/BoardConfigCommon.mk
 
+# Kernel
+TARGET_KERNEL_CONFIG += vendor/star_QGKI.config
+
 # Include proprietary files
 include vendor/xiaomi/mars/BoardConfigVendor.mk