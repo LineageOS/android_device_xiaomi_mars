6ebf07bb4855e03ea68f4aaa98fa1abd21e2a576
FlowerSea0208 <zzp060208@gmail.com>
2024-09-30 06:54:40 +0800

    Revert "mars: Import udfps long press sensor"
    This reverts commit 663caf353c724c5296635e130ca9b0e325e9b290.
    
    Change-Id: Idab87f6f70a7423253c41377c1a0a7676111f927

--- device.mk
+++ device.mk
@@ -29,7 +29,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    sensors.xiaomi.mars
+    sensors.xiaomi
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
