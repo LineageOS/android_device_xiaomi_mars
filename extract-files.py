bb0579f6884f85d576622a3ce36278b277b16f52
Giovanni Ricca <giovanniricca@proton.me>
2025-06-09 22:20:41 +0200

    mars: Target has udfps
    Change-Id: I4dd9ac72e1feb67a1c5a86c9254d57c586b5f0b9
    Signed-off-by: WilliamSado <williamsado1004@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -32,6 +32,8 @@ lib_fixups: lib_fixups_user_type = {
 blob_fixups: blob_fixups_user_type = {
     ('vendor/etc/camera/pureShot_parameter.xml', 'vendor/etc/camera/pureView_parameter.xml'): blob_fixup()
         .regex_replace(r'=(\d+)>', r'="\1">'),
+    'vendor/etc/sensors/hals.conf': blob_fixup()
+        .add_line_if_missing('sensors.xiaomi.v2.so'),
     'vendor/lib64/hw/camera.qcom.so': blob_fixup()
         .add_needed('libprocessgroup_shim.so')
         .binary_regex_replace(b'\x73\x74\x5F\x6C\x69\x63\x65\x6E\x73\x65\x2E\x6C\x69\x63', b'\x63\x61\x6D\x65\x72\x61\x5F\x63\x6E\x66\x2E\x74\x78\x74'),
