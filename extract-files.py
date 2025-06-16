c1b519bd3731e94e1e8dd6aa7d9c8d88f6f4ea5f
Bin <9633157+web1n@users.noreply.github.com>
2025-06-16 23:40:24 +0800

    mars: Patch audio to load libaudioroute-v34.so
    Change-Id: Idad20512982b8753820fffa6d956df137e462638

--- extract-files.py
+++ extract-files.py
@@ -38,7 +38,10 @@ blob_fixups: blob_fixups_user_type = {
         .add_line_if_missing('sensors.xiaomi.v2.so'),
     'vendor/lib/hw/audio.primary.mars.so': blob_fixup()
         .replace_needed('/vendor/lib/liba2dpoffload.so', '/odm/lib/liba2dpoffload.so')
-        .replace_needed('/vendor/lib/libssrec.so', '/odm/lib/libssrec.so'),
+        .replace_needed('/vendor/lib/libssrec.so', '/odm/lib/libssrec.so')
+        .replace_needed('libaudioroute.so', 'libaudioroute-v34.so'),
+    'vendor/lib/libaudioroute_ext.so': blob_fixup()
+        .replace_needed('libaudioroute.so', 'libaudioroute-v34.so'),
     'vendor/lib/libmmcamera_faceproc.so': blob_fixup()
         .clear_symbol_version('__aeabi_memcpy')
         .clear_symbol_version('__aeabi_memset')
