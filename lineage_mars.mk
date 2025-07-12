64f63c144930e60b1577861fb2801a2f7e63fa36
WilliamSado <williamsado1004@gmail.com>
2025-07-13 02:54:33 +0800

    mars: Update from star OS2.0.7.0.UKACNXM
    Change-Id: Id6babbe28d5019936c0a655d5188ae91aa6955d2

--- lineage_mars.mk
+++ lineage_mars.mk
@@ -16,9 +16,9 @@ PRODUCT_MODEL := M2102K1AC
 PRODUCT_NAME := lineage_mars
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="mars_global-user 14 UKQ1.231207.002 V816.0.11.0.UKAMIXM release-keys" \
-    BuildFingerprint=Xiaomi/mars_global/mars:14/UKQ1.231207.002/V816.0.11.0.UKAMIXM:user/release-keys \
+    BuildDesc="mars-user 14 UKQ1.240624.001 V2.0.7.0.UKACNXM release-keys" \
+    BuildFingerprint=Xiaomi/mars/mars:14/UKQ1.240624.001/V2.0.7.0.UKACNXM:user/release-keys \
     DeviceProduct=mars \
-    SystemName=mars_global
+    SystemName=mars
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
