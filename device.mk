c9af8e13c2b915f392344703146224ec5c34fa12
WilliamSado <williamsado1004@gmail.com>
2025-07-12 21:20:42 +0800

    mars: Switch Aperture overlay to RRO
    Change-Id: Ibc37743089422b148eb2ac1c352a71075cc58ea0

--- device.mk
+++ device.mk
@@ -14,6 +14,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
+    ApertureMars \
     FrameworksResMars \
     SettingsProviderResMars \
     SettingsResMars \
