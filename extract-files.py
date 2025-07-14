cf5f2ddd9c51880d1d10267627c1a94faa5a4420
Giovanni Ricca <giovanniricca@proton.me>
2025-07-14 12:52:43 +0200

    mars: Drop 32bit Camera stack
    Also drop the prebuilt libprotobuf cpp lib while we are there.
    
    Change-Id: I886efd1a0a87ca1e24a1e7a56647954fcf6c76ad

--- extract-files.py
+++ extract-files.py
@@ -45,6 +45,8 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libprocessgroup_shim.so'),
     'vendor/lib64/vendor.xiaomi.hardware.cameraperf@1.0-impl.so': blob_fixup()
         .sig_replace('21 00 80 52 7c 00 00 94', '21 00 80 52 1F 20 03 D5'),
+    ('vendor/lib64/vl53l5.hal@1.0.so', 'vendor/lib64/vl53l5.hal@2.0.so'): blob_fixup()
+        .replace_needed('libprotobuf-cpp-full-vendor-3.9.1.so', 'libprotobuf-cpp-full-3.9.1.so'),
 }  # fmt: skip
 
 
