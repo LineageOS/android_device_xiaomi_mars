39c7172f0fafcf5028be7086474e8e39672c7689
ItsVixano <giovanniricca@proton.me>
2025-09-30 21:59:38 +0200

    mars: Rename Audio HAL and prebuilt Audio extensions
    Change-Id: Icd4d076daa2b5d6eefcd022a75861448c01fb4cb

--- extract-files.py
+++ extract-files.py
@@ -36,6 +36,9 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('xml=version', 'xml version'),
     'vendor/etc/sensors/hals.conf': blob_fixup()
         .add_line_if_missing('sensors.xiaomi.v2.so'),
+    'vendor/lib/hw/audio.primary.mars.so': blob_fixup()
+        .replace_needed('/vendor/lib/liba2dpoffload.so', '/odm/lib/liba2dpoffload.so')
+        .replace_needed('/vendor/lib/libssrec.so', '/odm/lib/libssrec.so'),
     'vendor/lib/libmmcamera_faceproc.so': blob_fixup()
         .clear_symbol_version('__aeabi_memcpy')
         .clear_symbol_version('__aeabi_memset')
