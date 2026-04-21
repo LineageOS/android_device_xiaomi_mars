46b8bee75f8324fe16783ca9450a7a43b85a52ea
WilliamSado <williamsado1004@gmail.com>
2026-04-21 16:12:15 +0800

    mars: Commonize libmisight.so
    Change-Id: I6b487769e3d8b574f7ac189532bd0c1ba632e647

--- extract-files.py
+++ extract-files.py
@@ -74,8 +74,6 @@ blob_fixups: blob_fixups_user_type = {
         .clear_symbol_version('rpcmem_to_fd'),
     'vendor/lib64/libmialgoengine.so' : blob_fixup()
         .add_needed('libprocessgroup_shim.so'),
-    'vendor/lib64/libmisight.so': blob_fixup()
-        .add_needed('libjsoncpp_shim.so'),
     ('vendor/lib64/vl53l5.hal@1.0.so', 'vendor/lib64/vl53l5.hal@2.0.so'): blob_fixup()
         .replace_needed('libprotobuf-cpp-full-vendor-3.9.1.so', 'libprotobuf-cpp-full-3.9.1.so'),
     'vendor/lib64/vendor.xiaomi.hardware.cameraperf@1.0-impl.so': blob_fixup()
